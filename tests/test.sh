0105b8fb5b44198434fc853e341f9a1a214c0924
mattjmcnaughton <mattjmcnaughton@gmail.com>
2017-12-05 20:57:37 -0500

    Install pip packages in locally
    Install pip packages for the local user, instead of trying to install
    globally. Because of this change, we can run `pup-python` without
    requiring sudo.

--- tests/test.sh
+++ tests/test.sh
@@ -7,13 +7,24 @@ test::check_syntax() {
 }
 
 test::run_ansible() {
-  ansible-playbook playbook.yml -i 'localhost' -e '{"install_python3": "true", "install_python2": "true", "pip3_to_install": ["awscli", "mypy"], "pip2_to_install": ["pipenv"], "pip3_to_uninstall": [], "pip2_to_uninstall": []}'
+  ansible-playbook playbook.yml -i 'localhost' -e '{"install_python3": "true", "install_python2": "true", "pip3_to_install": ["awscli", "pylint", "mypy"], "pip2_to_install": ["pipenv"], "pip3_to_uninstall": ["pylint"], "pip2_to_uninstall": []}'
 }
 
 test::assert_output() {
+  # append ~/.local/bin to the path
+
+  export PATH=$PATH:~/.local/bin
+
   for program_file in python python3 pipenv mypy; do
     if ! which $program_file >/dev/null; then
       echo "$program_file is not installed"
+      exit 1
+    fi
+  done
+
+  for program_file in pylint; do
+    if which $program_file >/dev/null; then
+      echo "$program_file not installed"
       exit 1
     fi
   done
