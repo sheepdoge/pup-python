1779ec6969e6565cf1c611cbaa5e84ebae06e454
mattjmcnaughton <mattjmcnaughton@gmail.com>
2018-01-07 08:32:49 -0500

    Ensure pip3 packages in path
    Ensure that ~/.local/bin (the home of python3 user pip packages in Linux)
    is in the path through the given dotfile.

--- tests/test.sh
+++ tests/test.sh
@@ -3,11 +3,11 @@
 set -e
 
 test::check_syntax() {
-  ansible-playbook playbook.yml -i 'localhost' -e '{"install_python3": "true", "install_python2": "true", "pip3_to_install": ["awscli", "mypy"], "pip2_to_install": ["pipenv"], "pip3_to_uninstall": [], "pip2_to_uninstall": []}' --syntax-check
+  ansible-playbook playbook.yml -i 'localhost' -e '{"install_python3": "true", "install_python2": "true", "pip3_to_install": ["awscli", "mypy"], "pip2_to_install": ["pipenv"], "pip3_to_uninstall": [], "pip2_to_uninstall": [], "dotfile": "~/.bashrc"}' --syntax-check
 }
 
 test::run_ansible() {
-  ansible-playbook playbook.yml -i 'localhost' -e '{"install_python3": "true", "install_python2": "true", "pip3_to_install": ["awscli", "pylint", "mypy"], "pip2_to_install": ["pipenv"], "pip3_to_uninstall": ["pylint"], "pip2_to_uninstall": []}'
+  ansible-playbook playbook.yml -i 'localhost' -e '{"install_python3": "true", "install_python2": "true", "pip3_to_install": ["awscli", "pylint", "mypy"], "pip2_to_install": ["pipenv"], "pip3_to_uninstall": ["pylint"], "pip2_to_uninstall": [], "dotfile": "~/.bashrc"}'
 }
 
 test::assert_output() {
